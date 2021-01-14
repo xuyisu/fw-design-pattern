dae89117c57595ef55e44e287873c2e3eb41bbb8
xuyisu <964092342@qq.com>
2021-01-15 02:17:44 +0800

    门面模式

---
+++ fw-pattern-facade/src/main/java/com/yisu/design/pattern/facade/ClassC.java
@@ -0,0 +1,13 @@
+package com.yisu.design.pattern.facade;
+
+/**
+ * @author xuyisu
+ * @description 门面模式
+ * @date 2021/1/15
+ */
+public class ClassC {
+    public void doSomethingC(){
+        //业务逻辑
+        System.out.println("do C");
+    }
+}
--- fw-pattern-facade/src/main/java/com/yisu/design/pattern/facade/test.java
+++
@@ -1,9 +0,0 @@
-package com.yisu.design.pattern.facade;
-
-/**
- * @author xuyisu
- * @description //TODO
- * @date 2021/1/14
- */
-public class test {
-}
